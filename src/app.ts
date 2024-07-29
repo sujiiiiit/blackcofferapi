451b251000a39ca99582d20adf8c037a201809ed
Sujit Dwivedi <dwiwedisv@rknec.edu>
2024-07-29 11:50:39 +0530

    next

--- src/app.ts
+++ src/app.ts
@@ -116,8 +116,5 @@ app.get("/search/:id", async (req: Request, res: Response) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
 
 export default app;
